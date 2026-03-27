6b8ed23a88025ad2077cab1e87f4899ffb8c1b17
Talha Zafar <talhazafarmustafa@gmail.com>
2026-03-27 18:51:57 +0500

    File Updated

--- src/Sections/Hero20.jsx
+++ src/Sections/Hero20.jsx
@@ -4,14 +4,62 @@
 
 
 function Hero20() {
-  return (
-    <div>
+    return (
+        <div>
 
-        <div className=" tal">
+            <footer class="footer">
+                <div class="footer-container">
+
+                    <div class="footer-col">
+                        <h3>About Company</h3>
+                        <p>
+                            Stay strong and keep your path focused. Push yourself with determination and embrace every challenge.
+                            Take time to recover, train with purpose and enjoy the results.
+                        </p>
+
+                        <div class="social-icons">
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-google"></i>
+                            <i class="fab fa-linkedin-in"></i>
+                        </div>
+                    </div>
+
+                    <div class="footer-col">
+                        <h3>Follow Us on Twitter</h3>
+                        <p class="highlight">Tweets by @radiustheme</p>
+                    </div>
+
+                    <div class="footer-col">
+                        <h3>Flickr Photos</h3>
+                        <div class="gallery">
+                            <img src="https://picsum.photos/80?1" />
+                            <img src="https://picsum.photos/80?2" />
+                            <img src="https://picsum.photos/80?3" />
+                            <img src="https://picsum.photos/80?4" />
+                            <img src="https://picsum.photos/80?5" />
+                            <img src="https://picsum.photos/80?6" />
+                        </div>
+                    </div>
+
+                    <div class="footer-col">
+                        <h3>Corporate Office</h3>
+                        <p><i class="fa-solid fa-address-book book"></i> 44 New Design Street, rne 005</p>
+                        <p><i class="fa-solid fa-phone phone"></i> 01800433633</p>
+                        <p><i class="fa-regular fa-message message"></i> info@gymedge.com</p>
+                    </div>
+
+                </div>
+
+                <div class="footer-bottom">
+                    <p>© Copyright GymEdge 2026. All Right Reserved. Designed and Developed by Talha Zafar </p>
+                </div>
+
+
+            </footer>
+
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Hero20
